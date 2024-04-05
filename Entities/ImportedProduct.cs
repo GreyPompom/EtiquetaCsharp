fe9ca40b5b471a80ffd6c22c75991aa1ea667c22
Emely - Desenvolvimento 06 <desenvolvimento06@pauta.com.br>
2024-04-05 16:49:14 -0300

    full project

---
+++ Entities/ImportedProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EtiquetaCsharp.Entities
+{
+    class ImportedProduct: Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+        public ImportedProduct(string name, double price, double customs)
+            :base(name, price)
+        {
+            CustomsFee = customs;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " $ " + Price + " Customs fee: $ " + CustomsFee.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
